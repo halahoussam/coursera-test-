25a97ac05cabcb14e599f625da35b4dfe73cb97f
halahoussam <109960867+halahoussam@users.noreply.github.com>
2022-08-29 13:42:07 +0200

    Add files via upload

--- New folder/js/gallery.js
+++ New folder/js/gallery.js
@@ -1,30 +1,31 @@
-/*Name this external file gallery.js*/
-
-function upDate(previewPic){
- /* In this function you should 
+function upDate(previewPic) {
+  /* In this function you should 
     1) change the url for the background image of the div with the id = "image" 
-    to the source file of the preview image
-    
-    2) Change the text  of the div with the id = "image" 
+    to the source file of the preview image */
+
+  var image = previewPic.src;
+  document.getElementById("image").style.backgroundImage =
+    "url('" + image + "')";
+
+  /*2) Change the text  of the div with the id = "image" 
     to the alt text of the preview image 
     */
-  background-image:url("bacon1"); 
-  background-image:url("bacon2");
-  background-image:url("bacon3");
-  
-  
-	}
 
-	function unDo(){
-     /* In this function you should 
-    1) Update the url for the background image of the div with the id = "image" 
-    back to the orginal-image.  You can use the css code to see what that original URL was
-    
-    2) Change the text  of the div with the id = "image" 
-    back to the original text.  You can use the html code to see what that original text was
-    */
-		background-color:#cceecc;
-	}
-   function showProperties(element){
-      document.getElementById('message').innerHTML=element.alt;
-   }
+  var text = previewPic.alt;
+  document.getElementById("image").innerHTML = text;
+}
+
+function unDo() {
+  //    In this function you should
+  //     1) Update the url for the background image of the div with the id = "image"
+  //     back to the orginal-image.  You can use the css code to see what that original URL was
+
+  document.getElementById("image").style.backgroundImage =
+    "url('" + null + "')";
+
+  //     2) Change the text  of the div with the id = "image"
+  //     back to the original text.  You can use the html code to see what that original text was
+
+  document.getElementById("image").innerHTML =
+    "Hover over an image below to display here.";
+}
